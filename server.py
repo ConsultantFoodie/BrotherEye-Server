be0c70c085de10aac41fe19fc346e862d0a6ce53
ConsultantFoodie <hardikti@gmail.com>
2022-02-14 20:27:32 +0530

    Adds server functionality for audience-host communication

--- server.py
+++ server.py
@@ -7,6 +7,19 @@ import websockets
 import base64
 import cv2
 import numpy as np
+import json
+
+class Person:
+    def __init__(self, ws, email, role):
+        self.ws = ws
+        self.name = email.split("@")[0]
+        self.email = email
+        self.role = role
+        self.presence = 0
+        self.num_frames = 0
+
+    def __repr__(self):
+        return self.email
 
 def data_uri_to_cv2_img(uri):
     encoded_data = uri.split(',')[1]
@@ -14,40 +27,56 @@ def data_uri_to_cv2_img(uri):
     img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
     return img
 
-def calc_presence(img):
+def calc_presence(uri):
+    img = data_uri_to_cv2_img(uri)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.1, 4)
     
     return int(len(faces)>0)
 
-num=0
-presence=0
+MEETS = {}
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-async def hello(websocket, path):
+async def handler(websocket, path):
     async for message in websocket:
-        global num,presence
-        if len(message) > 20:
-            # print("NUM:", num)
-            # print(message)
-            # print("got image {}".format(num))
-            num += 1
-            img = data_uri_to_cv2_img(message)
-            presence += calc_presence(img)
-        else:
-            print(message)
-            print(presence, num)
-            await websocket.send(str(round(100*presence/num)))
-            presence, num = 0, 0
-        
-        # with open("./photos/img_{}.jpg".format(num),"wb") as f:
+        msg = json.loads(message)
+        if msg["event"] == "init":
+            print(msg)
+            if msg["meetCode"] not in MEETS.keys():
+                MEETS[msg["meetCode"]] = {"Host":[], "Audi":[]}
             
+            person = Person(websocket, msg["email"], msg["role"])
 
+            if msg["role"] == "Audi":
+                adder = {"event": "Add new","name": person.name}
+                for host in MEETS[msg["meetCode"]]["Host"]:
+                    await host.ws.send(json.dumps(adder))
+            else:
+                person.name = "Presentation Score"
 
-    
+            MEETS[msg["meetCode"]][msg["role"]].append(person)
+            del person
+
+            print(MEETS)
+        elif msg["event"] == "process":
+            for person in MEETS[msg["meetCode"]][msg["role"]]:
+                if person.email == msg["email"]:
+                    if not msg["end"]:
+                        person.num_frames += 1
+                        person.presence += calc_presence(msg["data"])
+                    else:
+                        print(person.email, person.presence, person.num_frames)
+                        response = {"event": "Update", "name": person.name, "presence": str(round(100*(person.presence)/(person.num_frames)))}
+                        await websocket.send(json.dumps(response))
+                        if person.role == "Audi":
+                            for host in MEETS[msg["meetCode"]]["Host"]:
+                                await host.ws.send(json.dumps(response))
+                        
+                        del response
+                        person.presence, person.num_frames = 0, 0
 
 async def main():
-    async with websockets.serve(hello, "localhost", 8000):
+    async with websockets.serve(handler, "", 8000):
         await asyncio.Future()  # run forever
 
 asyncio.run(main())
